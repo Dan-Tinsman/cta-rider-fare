9119a4cc5ba097164b1578372c316aea2fbcfccc
Dan-Home <dan.tinsman@outlook.com>
2022-09-09 22:16:00 -0400

    added soem test cases that should pass with a solution.

--- bin/fare.test.js
+++ bin/fare.test.js
@@ -8,4 +8,62 @@ describe("CTA Fare Logic Tests", () => {
         // assert
         expect(result).toStrictEqual([4]);
     });
+
+    test("Fare does not exist", () => {
+        // arrange and act
+        var result = fare.findPieces([1, 2, 3, 4, 5], 6)
+
+        // assert
+        expect(result).toStrictEqual([]);
+    });
+
+    test("Fare does exist with 2 Gaavo pieces", () => {
+        // arrange and act
+        var result = fare.findPieces([1, 2, 3, 4, 5], 7)
+
+        // add the result
+        let sum;
+        result.forEach(piece => {
+            sum += piece;
+        });
+
+        // assert
+        expect(sum).toBe(7);
+    });
+
+    test("Fare does exist with 3 Gaavo pieces", () => {
+        // arrange and act
+        var result = fare.findPieces([1, 2, 3, 4, 5], 12)
+
+        // add the result
+        let sum;
+        result.forEach(piece => {
+            sum += piece;
+        });
+
+        // assert
+        expect(sum).toBe(12);
+    });
+
+    test("Fare does exist with 2 partial Gaavo pieces", () => {
+        // arrange and act
+        var result = fare.findPieces([1.5, 2.33, 3, 4, 5], 3.83)
+
+        // add the result
+        let sum;
+        result.forEach(piece => {
+            sum += piece;
+        });
+
+        // assert
+        expect(sum).toBe(3.83);
+    });
+
+    test("Fare does exist with multiple pieces matching fare", () => {
+        // arrange and act
+        var result = fare.findPieces([7,7,7,7,7], 7);
+        
+        // assert
+        expect(result).toStrictEqual([7]);
+    });
 })
